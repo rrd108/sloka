2b76b4fe50616fc7f5375f88cdd91a1e98424011
rrd <rrd@webmania.cc>
2016-10-10 16:42:27 +0200

    get all 3 steps to work as expected

--- js/main.js
+++ js/main.js
@@ -1,12 +1,9 @@
 (function($){
-    var steps = 3;
-
-    var verse = 'ceto-darpaṇa-mārjanaṁ bhava-mahā-dāvāgni-nirvāpaṇaṁ' + "\n" +
+    var text = 'ceto-darpaṇa-mārjanaṁ bhava-mahā-dāvāgni-nirvāpaṇaṁ' + "\n" +
         'śreyaḥ-kairava-candrikā-vitaraṇaṁ vidyā-vadhū-jīvanam' + "\n" +
         'ānandāmbudhi-vardhanaṁ prati-padaṁ pūrṇāmṛtāsvādanaṁ' + "\n" +
-        'sarvātma-snapanaṁ paraṁ vijayate śrī-kṛṣṇa-saṅkīrtanam';
-
-    var translation = '„»Teljes győzelmet az Úr Kṛṣṇa szent neve éneklésének, ' +
+        'sarvātma-snapanaṁ paraṁ vijayate śrī-kṛṣṇa-saṅkīrtanam' + "\n" + "\n" +
+        '„»Teljes győzelmet az Úr Kṛṣṇa szent neve éneklésének, ' +
         'amely megtisztítja a szív tükrét, s véget vet az anyagi lét lángoló tüze ' +
         'okozta gyötrelmeknek! Ez az éneklés a telő hold, amely mindenkit az áldott ' +
         'szerencse fehér lótuszával ajándékoz meg. Ez minden műveltség élete és ' +
@@ -15,70 +12,94 @@
         'és képessé tesz bennünket arra, hogy minden pillanatban a tökéletes ' +
         'nektár ízét élvezhessük.«”';
 
-    if (localStorage.getItem('counter') === null) {
-        localStorage.setItem('counter', 1);
-    } else {
-        localStorage.setItem('counter', parseInt(localStorage.getItem('counter')) + 1);
+    if (localStorage.getItem('step') === null) {
+        localStorage.setItem('step', 1);
     }
 
-    if (localStorage.getItem('lap') === null) {
-        localStorage.setItem('lap', 1);
+    function makeAllTextVisible() {
+        //make all visible
+        text = text.replace(/<span class="invisible">/g, '');
+        text = text.replace(/<\/span>/g, '');
     }
 
-    if (localStorage.getItem('counter') > steps) {
-        localStorage.setItem('lap', parseInt(localStorage.getItem('lap')) + 1);
-        localStorage.setItem('counter', 1);
-    }
-
-    function step2() {
-        //make first few character visible of each words
+    function prepareVars() {
         var match, splitters = [];
         var splitPattern = /[ \-\n]/g;
-        //collect the indexes of split charaters into splitters array
-        while ((match = splitPattern.exec(verse)) != null) {
+        //collect the indexes of split characters into splitters array
+        while ((match = splitPattern.exec(text)) != null) {
             splitters.push(match.index);
         }
 
         var modifiedVerse = '';
-        var words = verse.split(splitPattern);
+        var words = text.split(splitPattern);
         var pos = 0;
-        $.each(words, function (index, word) {
-            // TODO words longer than steps will still shown up
-            var modifiedWord = '';
-            for (var i = 0; i < word.length; i++) {
-                if (i < (word.length - localStorage.getItem('counter')) //char pos is smaller than counter
-                    //|| i == (word.length - 1)                         //the last char
-                    // counter is over the length of this word, but this is the first character
-                    || (word.length <= localStorage.getItem('counter')
-                        && i == 0))
-                {
-                    modifiedWord += word[i];
-                } else {
-                    //this is a hidden char
-                    modifiedWord += '<span class="invisible">' + word[i] + '</span>';
-                    //modifiedWord += '<span style="color:#eee">' + word[i] + '</span>';
-                    // TODO 1 span per word would be better
-                }
-            }
-            //put splitters back
-            modifiedVerse += modifiedWord + (verse[splitters[pos]] ? verse[splitters[pos]] : '');
-            pos++;
-        });
+        return {
+            splitters: splitters,
+            modifiedVerse: modifiedVerse,
+            words: words,
+            pos: pos
+        };
+    }
 
-        verse = modifiedVerse;
-        updateMain();
+    function putSplittersBack(v, modifiedWord) {
+        v.modifiedVerse += modifiedWord + (text[v.splitters[v.pos]] ? text[v.splitters[v.pos]] : '');
+        v.pos++;
+    }
+
+    function makeHalfTextVisible() {
+        //make half of each words visible
+        makeAllTextVisible();
+        var v = prepareVars();
+        $.each(v.words, function (index, word) {
+            var spanPos = parseInt(word.length / 2);
+            var modifiedWord = word.substr(0, spanPos) +
+                '<span class="invisible">' +
+                word.substr(spanPos, word.length - spanPos) +
+                '</span>';
+            putSplittersBack(v, modifiedWord);
+        });
+        text = v.modifiedVerse;
+    }
+
+    function makeFirstCharVisible() {
+        //make only first letters visible
+        makeAllTextVisible();
+        var v = prepareVars();
+        $.each(v.words, function (index, word) {
+            var spanPos = parseInt(word.length - 1);
+            var modifiedWord = word.substr(0, 1) +
+                '<span class="invisible">' +
+                word.substr(1, spanPos) +
+                '</span>';
+            putSplittersBack(v, modifiedWord);
+        });
+        text = v.modifiedVerse;
     }
 
     function step(num) {
-        localStorage.setItem('lap', num);
-        if (num == 2) {
-            step2();
+        localStorage.setItem('step', num);
+        if (num == 1) {
+            makeAllTextVisible();
+        } else if (num == 2) {
+            makeHalfTextVisible();
+        } else if (num == 3) {
+            makeFirstCharVisible();
         }
+        updateMain();
     }
 
     function updateMain() {
-        verse = verse.replace(/(?:\n)/g, '<br>');
-        $('#main').html(verse + '<hr>' + translation);
+        $('#main').html(text.replace(/(?:\n)/g, '<br>'));
+    }
+
+    function addFilledToImgSrc(img) {
+        if (img.attr('src').search('filled') == -1) {
+            img.attr('src', img.attr('src').replace('.png', '-filled.png'));
+        }
+    }
+
+    function removeFilledFromImgSrc(img) {
+        img.attr('src', img.attr('src').replace('-filled', ''));
     }
 
     $(function() {
@@ -86,20 +107,20 @@
         $('nav img').click(function(event) {
             $('nav img').each(function() {
                 if (event.target == this) {
-                    var num = $(this).attr('src').replace(/\.png/, '');
-                    num = num.replace(/img\//, '');
-                    num = num.replace(/\-filled/, '');
+                    var num = $(this).attr('src').replace('.png', '');
+                    num = num.replace('img/', '');
+                    num = num.replace('-filled', '');
                     step(num);
-                    $(this).attr('src', $(this).attr('src').replace(/\.png/, '-filled.png'));
+                    addFilledToImgSrc($(this));
                 } else {
-                    $(this).attr('src', $(this).attr('src').replace(/\-filled/, ''));
+                    removeFilledFromImgSrc($(this));
                 }
             });
         });
 
-        $('#counter').text(localStorage.getItem('lap') + '/' + localStorage.getItem('counter'));
+        addFilledToImgSrc($('#s' + localStorage.getItem('step')));
 
-        updateMain();
+        step(localStorage.getItem('step'));
     });
 
 })(jQuery);
